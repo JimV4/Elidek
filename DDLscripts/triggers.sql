e2354a47bcf84b4efa88e2a7ac25c0e9bb62b934
Dimitris Vassiliou <dhmhtrhs.vassiliou@gmail.com>
2022-05-26 00:44:19 +0300

    update

--- DDLscripts/triggers.sql
+++ DDLscripts/triggers.sql
@@ -1,44 +1,67 @@
 /*check if a researcher works at a project that he evaluates*/
+drop trigger if exists integrity;
 delimiter //
 create trigger integrity after insert on works_at
 for each row
 begin
-	declare message_text varchar(20);
+	declare message_text varchar(50);
 
 	if new.researcher_ID = (select evaluator_ID
 								from projects
 								where new.project_ID = projects.project_ID)
 		
 	then 
-		signal sqlstate '45000' set message_text = 'Error';
+		signal sqlstate '45000' set message_text = 'Error! Researcher = Evaluator';
 	end if;
 end;//
 
 
-/*create trigger integrity after update on works_at
-referencing new row as nrow
+drop trigger if exists integrity_update;
+delimiter //
+create trigger integrity_update after update on works_at
 for each row
-when (nrow.researcher_ID = (select evaluator_ID
-							from projects
-							where nrow.project_ID = projects.project_ID))
-begin atomic
-	rollback
-end;
+begin
+	declare message_text varchar(50);
 
-/* make sure that when a project is inserted or updated, supervisor works at this project
-create trigger existence after insert on projects
-referencing new row as nrow
-for each row
-begin atomic
-	insert in works_at values (nrow.project_ID, nrow.supervisor_ID)
-end;
+	if new.researcher_ID = (select evaluator_ID
+								from projects
+								where new.project_ID = projects.project_ID)
+		
+	then 
+		signal sqlstate '45000' set message_text = 'Error! Researcher = Evaluator';
+	end if;
+end;//
 
-create trigger existence after update on projects
-referencing new row as nrow
+
+/*check if the evaluator of project is the same with supervisor*/
+drop trigger if exists integrity_projects;
+delimiter //
+create trigger integrity_projects after insert on projects
 for each row
-begin atomic
-	insert in works_at values (nrow.project_ID, nrow.supervisor_ID)
-end;
+begin
+	declare message_text varchar(50);
+
+	if new.supervisor_ID = new.evaluator_ID then 
+    
+		signal sqlstate '45000' set message_text = 'Error! Evaluator = Supervisor';
+        
+	end if;
+end;//
+
+
+drop trigger if exists integrity_projects_update;
+delimiter //
+create trigger integrity_projects_update after update on projects
+for each row
+begin
+	declare message_text varchar(50);
+
+	if new.supervisor_ID = new.evaluator_ID then 
+    
+		signal sqlstate '45000' set message_text = 'Error! Evaluator = Supervisor';
+        
+	end if;
+end;//
 
 
 
